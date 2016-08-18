98154f17949d203d4353d7e4b02a8331dd62c725
Dmitriy Arkhipov <dmitriy.arkhipov.biz@mail.ru>
2016-08-18 13:42:02 +0800

    Initial Commit

---
+++ composite_pattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  composite_pattern
+//
+//  Created by Dmitriy Arkhipov on 18.08.16.
+//  Copyright © 2016 Dmitriy Arkhipov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
